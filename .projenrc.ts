e62d5b6333d9c6f196b0b5bebad3c0d4620b04ad
d4ndel1on <stefan@fancynetwork.net>
2024-07-07 11:03:22 +0200

    npm run build

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,6 @@
 import { awscdk } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'd4ndel1on',
   authorName: 'Stefan',
@@ -6,12 +8,13 @@ const project = new awscdk.AwsCdkConstructLibrary({
   cdkVersion: '2.147.3',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
-  name: '@mia/core',
+  name: 'mia',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/d4ndel1on/mia.git',
   license: 'MIT',
   keywords: ['cognito', 'lambda', 'api-gateway', 'dynamodb'],
   description: 'Construct to integrate user management into a project',
   packageName: '@mia/core',
+  npmAccess: NpmAccess.PUBLIC,
 });
 project.synth();
